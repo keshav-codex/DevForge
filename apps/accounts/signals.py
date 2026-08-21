5f69330c152233412cfa524dde289c584e5fccfb
keshav-codex <kjkeshav0698@gmail.com>
2026-08-21 19:30:10 +0530

    accounts signal

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -5,11 +5,15 @@ from django.contrib.auth.models import User
 from allauth.socialaccount.models import SocialAccount
 
 from .models import Profile
+from .email_service import send_welcome_email
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-    profile, _ = Profile.objects.get_or_create(user=instance)
+
+    profile, profile_created = Profile.objects.get_or_create(
+        user=instance
+    )
 
     social_account = SocialAccount.objects.filter(
         user=instance,
@@ -17,19 +21,52 @@ def create_profile(sender, instance, created, **kwargs):
     ).first()
 
     if social_account:
+
         data = social_account.extra_data
 
         profile.google_id = social_account.uid
         profile.email = instance.email
-        profile.email_verified = data.get("email_verified", False)
-        profile.first_name = data.get("given_name", "")
-        profile.last_name = data.get("family_name", "")
+        profile.email_verified = data.get(
+            "email_verified",
+            False
+        )
+
+        profile.first_name = data.get(
+            "given_name",
+            ""
+        )
+
+        profile.last_name = data.get(
+            "family_name",
+            ""
+        )
+
         profile.full_name = data.get(
             "name",
             f"{profile.first_name} {profile.last_name}".strip()
         )
-        profile.profile_picture = data.get("picture", "")
-        profile.google_locale = data.get("locale", "")
-        profile.google_hosted_domain = data.get("hd", "")
+
+        profile.profile_picture = data.get(
+            "picture",
+            ""
+        )
+
+        profile.google_locale = data.get(
+            "locale",
+            ""
+        )
+
+        profile.google_hosted_domain = data.get(
+            "hd",
+            ""
+        )
 
         profile.save()
+
+        # Send welcome email only when
+        # the Profile was created for the first time.
+        if profile_created:
+
+            send_welcome_email(
+                profile
+            )
